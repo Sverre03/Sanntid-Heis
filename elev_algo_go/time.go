747f2100e572751d9d4c9a13dd8e076dc8e0d033
andreaszurcher <144330628+andreaszurcher@users.noreply.github.com>
2025-01-22 10:57:05 +0100

    Create time.go

---
+++ elev_algo_go/time.go
@@ -0,0 +1,25 @@
+import (
+    "time"
+)
+
+var (
+    timerEndTime time.Time
+    timerActive  bool
+)
+
+func getWallTime() time.Time {
+    return time.Now()
+}
+
+func timerStart(duration time.Duration) {
+    timerEndTime = getWallTime().Add(duration)
+    timerActive = true
+}
+
+func timerStop() {
+    timerActive = false
+}
+
+func timerTimedOut() bool {
+    return timerActive && getWallTime().After(timerEndTime)
+}

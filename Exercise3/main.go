31d2ee77694f1bf3b87b85684247707556291aa2
Sverre03 <sverre_stalsberg@yahoo.com>
2025-01-22 09:38:51 +0100

    added elevator source files

--- Exercise3/main.go
+++ Exercise3/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	// Create a new command
+	cmd := exec.Command("ls", "-l")
+
+	// Run the command
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Print the output
+	fmt.Println(string(output))
+}

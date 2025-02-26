e029ddc72b0d31b69a2c4f37e12338b30fa32db2
axelmf <axelmf@stud.ntnu.no>
2025-02-26 12:28:39 +0100

    Fix HAll ASsingment transmitter, design test for it

--- util/config/config.go
+++ util/config/config.go
@@ -7,7 +7,7 @@ import (
 const DOOR_OPEN_DURATION = 3 * time.Second
 const NUM_FLOORS = 4
 const NUM_BUTTONS = 3
-const MSG_ID_PARTITION_SIZE = 2 << 16
+const MSG_ID_PARTITION_SIZE = 2 << 12
 const CONNECTION_TIMEOUT = 500 * time.Millisecond
 const MASTER_TRANSMIT_INTERVAL = 50 * time.Millisecond
 const PORT_NUM = 20011

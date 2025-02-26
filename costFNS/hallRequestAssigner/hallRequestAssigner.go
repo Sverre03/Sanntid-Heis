cba68f5223c56dd38c4407bec3c7e545e4afabb4
Andreas Zurcher <andreas.zurcher@propulsentnu.no>
2025-02-26 11:48:54 +0100

    Fixed HRA assigner input function

--- costFNS/hallRequestAssigner/hallRequestAssigner.go
+++ costFNS/hallRequestAssigner/hallRequestAssigner.go
@@ -1,34 +1,44 @@
 package hallRequestAssigner
 
 import (
+	"elev/Network/network/messages"
+	"elev/elevator"
+	"elev/util/config"
 	"encoding/json"
 	"fmt"
 	"os/exec"
 	"runtime"
+	"strings"
 )
 
 // Struct members must be public in order to be accessible by json.Marshal/.Unmarshal
 // This means they must start with a capital letter, so we need to use field renaming struct tags to make them camelCase
 
 type HRAElevState struct {
-	Behavior    string `json:"behavior"`
-	Floor       int    `json:"floor"`
-	Direction   string `json:"direction"`
-	CabRequests []bool `json:"cabRequests"`
+	Behavior    string                  `json:"behavior"`
+	Floor       int                     `json:"floor"`
+	Direction   string                  `json:"direction"`
+	CabRequests [config.NUM_FLOORS]bool `json:"cabRequests"`
 }
 
 type HRAInput struct {
-	HallRequests [][2]bool               `json:"hallRequests"`
-	States       map[string]HRAElevState `json:"states"`
+	HallRequests [config.NUM_FLOORS][2]bool `json:"hallRequests"`
+	States       map[string]HRAElevState    `json:"states"`
 }
 
-func InputFunction(states []HRAElevState, HallRequests [][2]bool) HRAInput {
-	input := HRAInput{
-		HallRequests: HallRequests,
-		States:       make(map[string]HRAElevState),
+func InputFunction(allElevStates map[int]messages.ElevStates, hallRequests [config.NUM_FLOORS][2]bool) HRAInput {
+	allElevStatesInputFormat := make(map[string]HRAElevState)
+	for id, state := range allElevStates {
+		allElevStatesInputFormat[fmt.Sprintf("elevator%d", id)] = HRAElevState{
+			Behavior:    state.Behavior,
+			Floor:       state.Floor,
+			Direction:   strings.ToLower(elevator.MotorDirectionToString(state.Direction)),
+			CabRequests: state.CabRequest,
+		}
 	}
-	for i, state := range states {
-		input.States[fmt.Sprintf("elevator%d", i)] = state
+	input := HRAInput{
+		HallRequests: hallRequests,
+		States:       allElevStatesInputFormat,
 	}
 	return input
 }
@@ -50,7 +60,7 @@ func OutputFunction(input HRAInput) *map[string][][2]bool {
 		fmt.Println("json.Marshal error: ", err)
 		return nil
 	}
-	ret, err := exec.Command("../hallRequestAssigner/"+hraExecutable, "-i", string(jsonBytes)).CombinedOutput()
+	ret, err := exec.Command("costFNS/hallRequestAssigner/"+hraExecutable, "-i", string(jsonBytes)).CombinedOutput()
 	if err != nil {
 		fmt.Println("exec.Command error: ", err)
 		fmt.Println(string(ret))

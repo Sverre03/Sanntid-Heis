69e517abe397fb64a679ce680642f9823a35e220
Andreas Zurcher <andreas.zurcher@propulsentnu.no>
2025-03-06 13:23:48 +0100

    Master now able to control its own elevator

--- tests/test_FSM.go
+++ tests/test_FSM.go
@@ -1,21 +1,17 @@
 package tests
 
 import (
-	"elev/Network/network/messages"
-	"elev/elevator"
 	"elev/node"
-	"time"
 )
 
 func RunTestNode() {
-	Node1 := node.Node(0)
+	Node1 := node.Node(1)
 	go node.MasterProgram(Node1)
 
+	// Node1.ElevStatesTx <- messages.ElevStates{NodeID: 1, Direction: elevator.MD_Up, Behavior: "idle", Floor: 1, CabRequest: [4]bool{false, true, false, false}}
 
-	Node1.ElevStatesTx <- messages.ElevStates{NodeID: 1, Direction: elevator.MD_Up, Behavior: "idle", Floor: 1, CabRequest: [4]bool{false, true, false, false}}	
+	// time.Sleep(1 * time.Second)
 
-	time.Sleep(1 * time.Second)
-
-	newHallReq := messages.NewHallRequest{Floor: 2, HallButton: 0}
-	Node1.NewHallReqTx <- newHallReq
+	// newHallReq := messages.NewHallRequest{Floor: 2, HallButton: 0}
+	// Node1.NewHallReqTx <- newHallReq
 }

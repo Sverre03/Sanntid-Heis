0cc79374a096ed49d702a2012cacae988b80aed5
haavagr <haavagr@stud.ntnu.no>
2025-03-12 11:44:32 +0100

    move code

--- node/master.go
+++ node/master.go
@@ -4,9 +4,36 @@ import (
 	"elev/Network/messages"
 	"elev/costFNS/hallRequestAssigner"
 	"elev/elevator"
-	"elev/util/msgidbuffer"
 	"fmt"
 )
+
+const bufferSize = 5
+
+// A buffer that holds the last #buffersize message ids
+type MessageIDBuffer struct {
+	messageIDs [bufferSize]uint64
+	size       int
+	index      int
+}
+
+// using Add, you can add a message ID to the buffer. It overwrites in a FIFO manner
+func (buf *MessageIDBuffer) Add(id uint64) {
+	if buf.size == buf.index {
+		buf.index = 0
+	}
+	buf.messageIDs[buf.index] = id
+	buf.index += 1
+}
+
+// check if a message id is in the buffer
+func (buf *MessageIDBuffer) Contains(id uint64) bool {
+	for i := 0; i < buf.size; i++ {
+		if buf.messageIDs[i] == id {
+			return true
+		}
+	}
+	return false
+}
 
 func MasterProgram(node *NodeData) nodestate {
 	fmt.Printf("Node %d is now a Master\n", node.ID)
@@ -15,7 +42,7 @@ func MasterProgram(node *NodeData) nodestate {
 	activeNewHallReq := false
 	activeConnReq := make(map[int]messages.ConnectionReq)
 
-	var recentHACompleteBuffer msgidbuffer.MessageIDBuffer
+	var recentHACompleteBuffer MessageIDBuffer
 	var nextNodeState nodestate
 
 	node.GlobalHallRequestTx <- messages.GlobalHallRequest{HallRequests: node.GlobalHallRequests}

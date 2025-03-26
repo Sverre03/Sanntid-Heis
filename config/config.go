4145dfea5646e54d1d6bbb1ba374ce4113bd5177
Sverre03 <107675516+Sverre03@users.noreply.github.com>
2025-03-26 20:25:10 +0100

    Remove unused config constants

--- config/config.go
+++ config/config.go
@@ -11,20 +11,13 @@ const NUM_BUTTONS = 3
 const MSG_ID_PARTITION_SIZE = uint64(2 << 60)
 const MASTER_TRANSMIT_INTERVAL = 50 * time.Millisecond
 const ELEV_STATE_TRANSMIT_INTERVAL = 50 * time.Millisecond
-const NODE_DOOR_POLL_INTERVAL = 1000 * time.Millisecond
-const TIMEOUT_TIMER_POLL_INTERVAL = 50 * time.Millisecond
 
-const HALL_BUTTON_TO_NODE_DELAY = 50 * time.Millisecond
 const MASTER_CONNECTION_TIMEOUT = 500 * time.Millisecond
-const STATE_REQUEST_INTERVAL = 100 * time.Millisecond
 
 const ELEVATOR_STUCK_BETWEEN_FLOORS_TIMEOUT = 10 * time.Second
 const ELEVATOR_STUCK_BETWEEN_FLOORS_POLL_INTERVAL = 1 * time.Second
 
 const CONNECTION_REQ_INTERVAL = 100 * time.Millisecond
-const DISCONNECTED_DECISION_INTERVAL = 2000 * time.Millisecond
+const DISCONNECTED_DECISION_INTERVAL = 2 * time.Second
 const NODE_CONNECTION_TIMEOUT = 2 * time.Second
-const PORT_NUM = 20011
-const INPUT_POLL_INTERVAL = 25 * time.Millisecond
-const MASTER_TIMEOUT = 600 * time.Millisecond
 const PEER_POLL_INTERVAL = 20 * time.Millisecond
